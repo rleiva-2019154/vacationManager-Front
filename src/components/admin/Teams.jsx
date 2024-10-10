6480f270a719495827b46db6d65fe84c950bc4ca
rleiva-2019154 <rleiva-2019154@kinal.edu.gt>
2024-10-10 17:46:05 -0600

    Creando vista para asignar role

---
+++ src/components/admin/Teams.jsx
@@ -0,0 +1,19 @@
+import { Footer } from "../Footer";
+import { Navbar } from "../Navbar";
+
+export const Teams = () => {
+
+    return (
+        <div className="flex flex-col bg-slate-100 h-screen overflow-hidden">
+            <Navbar className="fixed top-0 left-0 right-0 h-16" />
+            <div className="flex-1 overflow-auto p-4">
+                <div className="flex flex-col flex-1 items-center mt-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    Teams
+                    </div>
+                </div>
+            </div>
+            <Footer className="fixed bottom-0 left-0 right-0 h-16" />
+        </div>
+    );
+};
